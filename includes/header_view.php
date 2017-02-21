0b184102fae8814ca724f30b6b55ea14fc47c32e
SirXploSiv <joel.garcia9510@gmail.com>
2017-02-21 11:59:15 +0100

    RemoveIndex&Template
    RemoveIndex&Template

---
+++ includes/header_view.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+    <head>
+        <title>CI Encuesta Sigma Learning Center | Sant Joan</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/bootstrap.min.css')?>" />
+        <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/style.css')?>" />
+    </head>
+    <body>
